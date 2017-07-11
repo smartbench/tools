350cc8339a7e90c4d1037681947bdfbcdf33c9c9
kuku22 <kuku2206@gmail.com>
2017-07-11 20:29:20 -0300

    ejemplo de escritura para Bisfifo

--- ftdi/eeprom_rw/read_write/main.c
+++ ftdi/eeprom_rw/read_write/main.c
@@ -9,6 +9,8 @@
 #include <sys/time.h>
 #include "ftd2xx.h"
 
+int printData(FT_DEVICE ftDevice, FT_PROGRAM_DATA * Data);
+
 int main(int argc, char *argv[])
 {
 	FT_STATUS	ftStatus;
@@ -21,21 +23,13 @@ int main(int argc, char *argv[])
 
 	ftStatus = FT_GetLibraryVersion(&libraryVersion);
 	if (ftStatus == FT_OK)
-	{
 		printf("Library version = 0x%x\n", (unsigned int)libraryVersion);
-	}
-	else
-	{
-		printf("Error reading library version.\n");
-		return 1;
+	else{
+		printf("Error reading library version.\n"); return 1;
 	}
 	
-	if(argc > 1) {
-		sscanf(argv[1], "%d", &iport);
-	}
-	else {
-		iport = 0;
-	}
+	if(argc > 1) sscanf(argv[1], "%d", &iport);
+	else iport = 0;
 	printf("Opening port %d\n", iport);
 	
 	ftStatus = FT_Open(iport, &ftHandle0);
@@ -46,23 +40,20 @@ int main(int argc, char *argv[])
 			also rmmod usbserial
 		 */
 		printf("FT_Open(%d) failed\n", iport);
+		printf( "This can fail if the ftdi_sio driver is loaded "
+		 	    "use lsmod to check this and rmmod ftdi_sio to remove "
+		 	    "also rmmod usbserial");
 		return 1;
 	}
 	
 	printf("FT_Open succeeded.  Handle is %p\n", ftHandle0);
 
-	ftStatus = FT_GetDeviceInfo(ftHandle0,
-	                            &ftDevice,
-	                            NULL,
-	                            NULL,
-	                            NULL,
-	                            NULL); 
-	if (ftStatus != FT_OK) 
-	{ 
+	ftStatus = FT_GetDeviceInfo(ftHandle0, &ftDevice, NULL, NULL, NULL, NULL);
+	if (ftStatus != FT_OK){ 
 		printf("FT_GetDeviceType FAILED!\n");
 		retCode = 1;
 		goto exit;
-	}  
+	}
 
 	printf("FT_GetDeviceInfo succeeded.  Device is type %d.\n", 
 	       (int)ftDevice);
@@ -74,117 +65,46 @@ int main(int argc, char *argv[])
 	Data.ManufacturerId = (char *)malloc(256); /* E.g. "FT" */
 	Data.Description = (char *)malloc(256); /* E.g. "USB HS Serial Converter" */
 	Data.SerialNumber = (char *)malloc(256); /* E.g. "FT000001" if fixed, or NULL */
-	if (Data.Manufacturer == NULL ||
-	    Data.ManufacturerId == NULL ||
-	    Data.Description == NULL ||
-	    Data.SerialNumber == NULL)
-	{
+	if (Data.Manufacturer == NULL || Data.ManufacturerId == NULL || Data.Description == NULL || Data.SerialNumber == NULL){
 		printf("Failed to allocate memory.\n");
 		retCode = 1;
 		goto exit;
 	}
 
+    // LECTURA DE LA EEPROM
 	ftStatus = FT_EE_Read(ftHandle0, &Data);
 	if(ftStatus != FT_OK) {
 		printf("FT_EE_Read failed\n");
 		retCode = 1;
 		goto exit;
 	}
-
 	printf("FT_EE_Read succeeded.\n\n");
-		
-	printf("Signature1 = %d\n", (int)Data.Signature1);			
-	printf("Signature2 = %d\n", (int)Data.Signature2);			
-	printf("Version = %d\n", (int)Data.Version);				
-								
-	printf("VendorId = 0x%04X\n", Data.VendorId);				
-	printf("ProductId = 0x%04X\n", Data.ProductId);
-	printf("Manufacturer = %s\n", Data.Manufacturer);			
-	printf("ManufacturerId = %s\n", Data.ManufacturerId);		
-	printf("Description = %s\n", Data.Description);			
-	printf("SerialNumber = %s\n", Data.SerialNumber);			
-	printf("MaxPower = %d\n", Data.MaxPower);				
-	printf("PnP = %d\n", Data.PnP) ;					
-	printf("SelfPowered = %d\n", Data.SelfPowered);			
-	printf("RemoteWakeup = %d\n", Data.RemoteWakeup);			
-
-	if (ftDevice == FT_DEVICE_BM)
-	{
-		/* Rev4 (FT232B) extensions */
-		printf("BM:\n");
-		printf("---\n");
-		printf("\tRev4 = 0x%X\n", Data.Rev4);					
-		printf("\tIsoIn = 0x%X\n", Data.IsoIn);				
-		printf("\tIsoOut = 0x%X\n", Data.IsoOut);				
-		printf("\tPullDownEnable = 0x%X\n", Data.PullDownEnable);		
-		printf("\tSerNumEnable = 0x%X\n", Data.SerNumEnable);			
-		printf("\tUSBVersionEnable = 0x%X\n", Data.USBVersionEnable);		
-		printf("\tUSBVersion = 0x%X\n", Data.USBVersion);
-	}
-
-	if (ftDevice == FT_DEVICE_2232C) 
-	{
-		/* Rev 5 (FT2232C) extensions */
-		printf("2232RC:\n");
-		printf("-------\n");
-		printf("\tRev5 = 0x%X\n", Data.Rev5);					
-		printf("\tIsoInA = 0x%X\n", Data.IsoInA);				
-		printf("\tIsoInB = 0x%X\n", Data.IsoInB);				
-		printf("\tIsoOutA = 0x%X\n", Data.IsoOutA);				
-		printf("\tIsoOutB = 0x%X\n", Data.IsoOutB);				
-		printf("\tPullDownEnable5 = 0x%X\n", Data.PullDownEnable5);		
-		printf("\tSerNumEnable5 = 0x%X\n", Data.SerNumEnable5);		
-		printf("\tUSBVersionEnable5 = 0x%X\n", Data.USBVersionEnable5);	
-		printf("\tUSBVersion5 = 0x%X\n", Data.USBVersion5);			
-		printf("\tAIsHighCurrent = 0x%X\n", Data.AIsHighCurrent);		
-		printf("\tBIsHighCurrent = 0x%X\n", Data.BIsHighCurrent);		
-		printf("\tIFAIsFifo = 0x%X\n", Data.IFAIsFifo);			
-		printf("\tIFAIsFifoTar = 0x%X\n", Data.IFAIsFifoTar);			
-		printf("\tIFAIsFastSer = 0x%X\n", Data.IFAIsFastSer);			
-		printf("\tAIsVCP = 0x%X\n", Data.AIsVCP);				
-		printf("\tIFBIsFifo = 0x%X\n", Data.IFBIsFifo);			
-		printf("\tIFBIsFifoTar = 0x%X\n", Data.IFBIsFifoTar);			
-		printf("\tIFBIsFastSer = 0x%X\n", Data.IFBIsFastSer);			
-		printf("\tBIsVCP = 0x%X\n", Data.BIsVCP);
-	}
-
-	if (ftDevice == FT_DEVICE_232R)
-	{
-		/* Rev 6 (FT232R) extensions */
-		printf("232R:\n");
-		printf("-----\n");
-		printf("\tUseExtOsc = 0x%X\n", Data.UseExtOsc);			// Use External Oscillator
-		printf("\tHighDriveIOs = 0x%X\n", Data.HighDriveIOs);			// High Drive I/Os
-		printf("\tEndpointSize = 0x%X\n", Data.EndpointSize);			// Endpoint size
-
-		printf("\tPullDownEnableR = 0x%X\n", Data.PullDownEnableR);		// non-zero if pull down enabled
-		printf("\tSerNumEnableR = 0x%X\n", Data.SerNumEnableR);		// non-zero if serial number to be used
-
-		printf("\tInvertTXD = 0x%X\n", Data.InvertTXD);			// non-zero if invert TXD
-		printf("\tInvertRXD = 0x%X\n", Data.InvertRXD);			// non-zero if invert RXD
-		printf("\tInvertRTS = 0x%X\n", Data.InvertRTS);			// non-zero if invert RTS
-		printf("\tInvertCTS = 0x%X\n", Data.InvertCTS);			// non-zero if invert CTS
-		printf("\tInvertDTR = 0x%X\n", Data.InvertDTR);			// non-zero if invert DTR
-		printf("\tInvertDSR = 0x%X\n", Data.InvertDSR);			// non-zero if invert DSR
-		printf("\tInvertDCD = 0x%X\n", Data.InvertDCD);			// non-zero if invert DCD
-		printf("\tInvertRI = 0x%X\n", Data.InvertRI);				// non-zero if invert RI
-
-		printf("\tCbus0 = 0x%X\n", Data.Cbus0);				// Cbus Mux control
-		printf("\tCbus1 = 0x%X\n", Data.Cbus1);				// Cbus Mux control
-		printf("\tCbus2 = 0x%X\n", Data.Cbus2);				// Cbus Mux control
-		printf("\tCbus3 = 0x%X\n", Data.Cbus3);				// Cbus Mux control
-		printf("\tCbus4 = 0x%X\n", Data.Cbus4);				// Cbus Mux control
-
-		printf("\tRIsD2XX = 0x%X\n", Data.RIsD2XX); // non-zero if using D2XX
-	}	
+	
+	// IMPRIMO LO LEIDO EN PANTALLA
+    printData(ftDevice, &Data);
     
-    Data.MaxPower = 500;//0xFA;
+    // CAMBIO LOS VALORES QUE ME PINTA
+    Data.MaxPower = 100 + (Data.MaxPower - 50) % 400;//0xFA;
+    Data.IFBIsFifo7 = 1;
+    // Data.IFBIsFifoTar7 = 0;
 
+    // ESCRIBO LA ESTRUCTURA MODIFICADA
 	ftStatus = FT_EE_Program(ftHandle0, &Data);
 	if(ftStatus != FT_OK) {
 		printf("FT_EE_Program failed (%d)\n", (int)ftStatus);
 //		FT_Close(ftHandle0);
 	}
+	
+	// VUELVO A LEER LA EEPROM PARA VERIFICAR QUE SE
+	// ESCRIBIO CORRECTAMENTE.
+	ftStatus = FT_EE_Read(ftHandle0, &Data);
+	if(ftStatus != FT_OK) {
+		printf("FT_EE_Read failed\n");
+		retCode = 1;
+		goto exit;
+	}
+	printf("FT_EE_Read succeeded.\n\n");
+    printData(ftDevice, &Data);
     
     
 	
@@ -198,52 +118,56 @@ exit:
 	return retCode;
 }
 
+int printData(FT_DEVICE ftDevice, FT_PROGRAM_DATA * Data){
 
-/*
---------------------------------
-// Lógica de eeprom-read.c
---------------------------------
+/*	printf("Signature1 = %d\n", (int)Data->Signature1);			
+	printf("Signature2 = %d\n", (int)Data->Signature2);			
+	printf("Version = %d\n", (int)Data->Version);				
+								
+	printf("VendorId = 0x%04X\n", Data->VendorId);				
+	printf("ProductId = 0x%04X\n", Data->ProductId);
+	printf("Manufacturer = %s\n", Data->Manufacturer);			
+	printf("ManufacturerId = %s\n", Data->ManufacturerId);		
+	printf("Description = %s\n", Data->Description);			
+	printf("SerialNumber = %s\n", Data->SerialNumber);			*/
+	printf("MaxPower = %d\n", Data->MaxPower);				/*
+	printf("PnP = %d\n", Data->PnP) ;					
+	printf("SelfPowered = %d\n", Data->SelfPowered);			
+	printf("RemoteWakeup = %d\n", Data->RemoteWakeup);			*/
 
-main(){
-    ftStatus = FT_CreateDeviceInfoList(&numDevs);
-    ftStatus = FT_GetDeviceInfoList(devInfo, &numDevs);
-    //cargados: devInfo[i].Flags, .ID, .LocId, .SerialNumber, .Description, .ftHandle, .Type
-    readEeprom(devInfo[i].LocId, devInfo[i].Type);
+	if (ftDevice == FT_DEVICE_2232H){
+/*	    printf("2232H:\n");
+	    printf("-----\n");
+	    
+	    printf("\tPullDownEnable7 = 0x%X\n", Data->PullDownEnable7 );		// non-zero if pull down enabled
+	    printf("\tSerNumEnable7 = 0x%X\n", Data->SerNumEnable7 );	// non-zero if serial number to be used
+	    printf("\tALSlowSlew = 0x%X\n", Data->ALSlowSlew );		// non-zero if AL pins have slow slew
+	    printf("\tALSchmittInput = 0x%X\n", Data->ALSchmittInput );	// non-zero if AL pins are Schmitt input
+	    printf("\tALDriveCurrent = 0x%X\n", Data->ALDriveCurrent );	// valid values are 4mA, 8mA, 12mA, 16mA
+	    printf("\tAHSlowSlew = 0x%X\n", Data->AHSlowSlew );		// non-zero if AH pins have slow slew
+	    printf("\tAHSchmittInput = 0x%X\n", Data->AHSchmittInput );	// non-zero if AH pins are Schmitt input
+	    printf("\tAHDriveCurrent = 0x%X\n", Data->AHDriveCurrent );	// valid values are 4mA, 8mA, 12mA, 16mA
+	    printf("\tBLSlowSlew = 0x%X\n", Data->BLSlowSlew );		// non-zero if BL pins have slow slew
+	    printf("\tBLSchmittInput = 0x%X\n", Data->BLSchmittInput );	// non-zero if BL pins are Schmitt input
+	    printf("\tBLDriveCurrent = 0x%X\n", Data->BLDriveCurrent );	// valid values are 4mA, 8mA, 12mA, 16mA
+	    printf("\tBHSlowSlew = 0x%X\n", Data->BHSlowSlew );		// non-zero if BH pins have slow slew
+	    printf("\tBHSchmittInput = 0x%X\n", Data->BHSchmittInput );	// non-zero if BH pins are Schmitt input
+	    printf("\tBHDriveCurrent = 0x%X\n", Data->BHDriveCurrent );	// valid values are 4mA, 8mA, 12mA, 16mA
+	    */
+	    printf("\tIFAIsFifo7 = 0x%X\n", Data->IFAIsFifo7 );		// non-zero if interface is 245 FIFO
+	    printf("\tIFAIsFifoTar7 = 0x%X\n", Data->IFAIsFifoTar7 );	// non-zero if interface is 245 FIFO CPU target
+	    /*printf("\tIFAIsFastSer7 = 0x%X\n", Data->IFAIsFastSer7 );	// non-zero if interface is Fast serial
+	    printf("\tAIsVCP7 = 0x%X\n", Data->AIsVCP7 );			// non-zero if interface is to use VCP drivers
+	    */
+	    printf("\tIFBIsFifo7 = 0x%X\n", Data->IFBIsFifo7 );		// non-zero if interface is 245 FIFO
+	    printf("\tIFBIsFifoTar7 = 0x%X\n", Data->IFBIsFifoTar7 );	// non-zero if interface is 245 FIFO CPU target
+	    /*
+	    printf("\tIFBIsFastSer7 = 0x%X\n", Data->IFBIsFastSer7 );	// non-zero if interface is Fast serial
+	    printf("\tBIsVCP7 = 0x%X\n", Data->BIsVCP7 );			        // non-zero if interface is to use VCP drivers
+        printf("\tPowerSaveEnable = 0x%X\n", Data->PowerSaveEnable );   // non-zero if using BCBUS7 to save power for self-powered designs
+	    */
+	}
+	
+	return 0;
+
 }
-
-readEeprom(locationId, deviceType){
-    ftStatus = FT_OpenEx((PVOID)(uintptr_t)locationId, FT_OPEN_BY_LOCATION, &ftHandle);
-    header = (FT_EEPROM_HEADER *)eeprom;
-    header->deviceType = deviceType;
-    // Leo datos (todos?)
-    ftStatus = FT_EEPROM_Read(ftHandle, eeprom, sizeof(*eeprom), manufacturer, manufacturerId, description, serialNumber);
-    // cargados: manfactures, manfufacturerID, description, serialNumber, dualH(estructura FT2232H)
-    displayHeader((FT_EEPROM_HEADER *)eeprom);
-    if(deviceType == FT_DEVICE_2232H){
-        //displayDualH(&eeprom->dualH);
-        // cargada estructura del FT2232H:
-        // ...
-        // (eeprom->dualH).ADriverType
-        // (eeprom->dualH).BDriverType
-        // ...
-    }
-}
-
-//--------------------------------
-// Lógica de main.c (read)
-//--------------------------------
-
-main(){
-    ftStatus = FT_GetLibraryVersion(&libraryVersion);
-    ftStatus = FT_Open(iport, &ftHandle0);
-    ftStatus = FT_GetDeviceInfo(ftHandle0, &ftDevice, NULL, NULL, NULL, NULL);
-    // cargado: (int)ftDevice
-    ftStatus = FT_EE_Read(ftHandle0, &Data);
-    // cargado: estructura data
-    //  data.VendorId; .Description; .MaxPower; .SelfPowered; .PnP; etc...
-    //
-}
-
-
-
-*/
